97cd4692e36743867a8e69bc8af65644d9df74e4
Vikash Kumar <vikas.kumar@mitratech.com>
2023-05-07 13:57:54 +0530

    Added SOLID principles

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# SystemDesign
-System Design Concepts with Examples
+# Design Patterns and Principles
+
+## SOLID Design Principles
+
+    - Introduced by Robert C. Martin
+
+    1. S - Single Responsibility Principle (SOC - Separation of Concerns)
+    2. O - Open-Closed Principle
+    3. L - Liskov Substitution Principle
+    4. I - Interface Segregation Principle
+    5. D - Dependency Inversion Principle
+
+## Design Pattenrs
+
+    1. Creational Design Patterns
+        * Builder
+        * Factories
+            . Abstract Factory
+            . Factory Method
+        * Prototype
+        * Singlton
+    2. Structural Design Patterns
+        * Adapter
+        * Bridge
+        * Composite
+        * Decorator
+        * Facade
+        * Flyweight
+        * Proxy
+    3. Behavioral Design Patterns
+        * Chain of Responsibility
+        * Command
+        * Interpreter
+        * Iterator
+        * Mediator
+        * Memento
+        * Observer
+        * State
+        * Strategy
+        * Template Method
+        * Visitor
--- SOLID/single_responsibility.py
+++ SOLID/single_responsibility.py
@@ -1,1 +1,50 @@
-# Single Responsibility Principle
+"""
+
+    Single Responsibility Principle
+    - A class should only have one reason to change
+    - Separation of concerns - Different classes handling different, independent tasks/problems
+
+"""
+
+
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.entries.append(f"{self.count}: {text}")
+        self.count += 1
+
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        return "\n".join(self.entries)
+
+
+    # break SRP (Write journals in a file to presist)
+    # def save(self, filename):
+    #     file = open(filename, "w")
+    #     file.write(str(self))
+    #     file.close()
+
+class PersistenceManager:
+    def save_to_file(journal, filename):
+        file = open(filename, "w")
+        file.write(str(journal))
+        file.close()
+
+
+j = Journal()
+j.add_entry("I cried today.")
+j.add_entry("I ate a bug.")
+print(f"Journal entries:\n{j}\n")
+
+p = PersistenceManager()
+file = 'journal.txt'
+p.save_to_file(j, file)
+
+# verify!
+with open(file) as fh:
+    print(fh.read())
